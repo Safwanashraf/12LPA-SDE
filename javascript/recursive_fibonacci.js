77de8d77ed0e3f5bf7651798df478dc59212882e
Safwan <1.safwanashraf@gmail.com>
2026-08-12 14:31:48 +0530

    Recursive Fibonacci Program

---
+++ javascript/recursive_fibonacci.js
@@ -0,0 +1,12 @@
+function fib(n) {
+    // Base Cases:
+    if (n == 0) return 0;
+    if (n == 1) return 1;
+
+    // Recursive Case:
+    return fib(n - 1) + fib(n - 2);
+}
+
+console.log(fib(5));
+console.log(fib(10));
+
