74a45ae98c3929e391a0882c16e169152c04c1dd
Safwan <1.safwanashraf@gmail.com>
2026-08-19 23:47:41 +0530

    Updated some file structure

--- javascript/try_catch.js
+++ javascript/try_catch.js
@@ -6,10 +6,10 @@ function addTwoNums(a,b) {
             throw new ReferenceError('the second argument is not a number')
         } else {
             console.log(a + b)
-        }
+        };
     } catch(err) {
         console.log("Error!", err)
     }
 }
-addTwoNums(5, "5")
-console.log("It still works")
+addTwoNums(5, "5");
+console.log("It still works");
